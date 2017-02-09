110ca3b30e05e550e8c01c4293b7f6ab6a10c95e
DESKTOP-8UHTRAU\Slave-Joan <gotnosouls@gmail.com>
2017-02-10 00:24:53 +0100

    The beginning

---
+++ js/javascript.js
@@ -0,0 +1,155 @@
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+$(window).scroll(function() {
+  if($(this).scrollTop() > 150){
+    $('nav').addClass("sticky");
+  } else {
+    $('nav').removeClass("sticky");
+  }
+});
+
+
+
+
+$.getJSON('https://www.wowprogress.com/guild/us/sen-jin/Premonition/rating.tier8.ach/json_rank?callback=?', function(data) {
+  var temp;
+  temp = data.world_rank;
+
+  console.log(temp);
+});
+
+$.getJSON('https://www.warcraftlogs.com:443/v1/reports/guild/Memento/Auchindoun/EU?api_key=d59793255903969b4685a0a62961310d', function(data) {
+  var logs;
+  logs = data;
+  var log_name = [];
+  var log_link = []
+  var q = 0;
+
+  for (var i = logs.length-10; i < logs.length; i++) {
+    log_name[q] = logs[i].title;
+    log_link[q] = logs[i].id;
+    q++
+  }
+
+  $.each(
+    log_name,
+    function(i,v) {
+      $(".log_links").append("<li><a href='https://www.warcraftlogs.com/reports/" + log_link[i] + "' target='_blank'>" + v + "</a></li>");
+    }
+  );
+});
+
+
+/* Raid roster API from battle.net */
+$.getJSON('https://eu.api.battle.net/wow/guild/Auchindoun/Memento?fields=members&locale=en_GB&apikey=uq4auwygdasmyzrugxard2gvejc4xywq', function(data) {
+  var roster;
+  var name_ = [];
+  var class_ = [];
+  var spec = [];
+  var rank_ = [];
+  var j = 0;
+  roster = data;
+  /*Checking for players ranking and class*/
+  for (var i = 0; i < roster.members.length; i++) {
+    if(roster.members[i].rank < 5) {
+      class_[j] = roster.members[i].character.class;
+      spec[j] = roster.members[i].character.spec.name;
+      name_[j] = roster.members[i].character.name;
+      rank_[j] = roster.members[i].rank;
+      ++j;
+    }
+  }
+  $.each(
+    name_,
+    function(i,v) {
+      if (v != "Nightmãre") {
+        if(rank_[i] == 4) {
+          $(".trial_raiders").append("<li class='_" + class_[i] + "'>" + v + "</li>");
+        } else {
+          $(".core_raiders").append("<li class='_" + class_[i] + "'>" + v + "</li>");
+        }
+      }
+    }
+  );
+});
+
+/*SLider*/
+jQuery(document).ready(function ($) {
+
+    var jssor_1_SlideshowTransitions = [
+      {$Duration:1200,x:0.3,$During:{$Left:[0.3,0.7]},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+      {$Duration:1200,x:-0.3,$SlideOut:true,$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+      {$Duration:1200,x:-0.3,$During:{$Left:[0.3,0.7]},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+      {$Duration:1200,x:0.3,$SlideOut:true,$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+      {$Duration:1200,y:0.3,$During:{$Top:[0.3,0.7]},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+      {$Duration:1200,y:-0.3,$SlideOut:true,$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+      {$Duration:1200,y:-0.3,$During:{$Top:[0.3,0.7]},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+      {$Duration:1200,y:0.3,$SlideOut:true,$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+      {$Duration:1200,x:0.3,$Cols:2,$During:{$Left:[0.3,0.7]},$ChessMode:{$Column:3},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+      {$Duration:1200,x:0.3,$Cols:2,$SlideOut:true,$ChessMode:{$Column:3},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+      {$Duration:1200,y:0.3,$Rows:2,$During:{$Top:[0.3,0.7]},$ChessMode:{$Row:12},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+      {$Duration:1200,y:0.3,$Rows:2,$SlideOut:true,$ChessMode:{$Row:12},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+      {$Duration:1200,y:0.3,$Cols:2,$During:{$Top:[0.3,0.7]},$ChessMode:{$Column:12},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+      {$Duration:1200,y:-0.3,$Cols:2,$SlideOut:true,$ChessMode:{$Column:12},$Easing:{$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+      {$Duration:1200,x:0.3,$Rows:2,$During:{$Left:[0.3,0.7]},$ChessMode:{$Row:3},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+      {$Duration:1200,x:-0.3,$Rows:2,$SlideOut:true,$ChessMode:{$Row:3},$Easing:{$Left:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+      {$Duration:1200,x:0.3,y:0.3,$Cols:2,$Rows:2,$During:{$Left:[0.3,0.7],$Top:[0.3,0.7]},$ChessMode:{$Column:3,$Row:12},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+      {$Duration:1200,x:0.3,y:0.3,$Cols:2,$Rows:2,$During:{$Left:[0.3,0.7],$Top:[0.3,0.7]},$SlideOut:true,$ChessMode:{$Column:3,$Row:12},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+      {$Duration:1200,$Delay:20,$Clip:3,$Assembly:260,$Easing:{$Clip:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+      {$Duration:1200,$Delay:20,$Clip:3,$SlideOut:true,$Assembly:260,$Easing:{$Clip:$Jease$.$OutCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+      {$Duration:1200,$Delay:20,$Clip:12,$Assembly:260,$Easing:{$Clip:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+      {$Duration:1200,$Delay:20,$Clip:12,$SlideOut:true,$Assembly:260,$Easing:{$Clip:$Jease$.$OutCubic,$Opacity:$Jease$.$Linear},$Opacity:2}
+    ];
+
+    var jssor_1_options = {
+      $AutoPlay: true,
+      $SlideshowOptions: {
+        $Class: $JssorSlideshowRunner$,
+        $Transitions: jssor_1_SlideshowTransitions,
+        $TransitionsOrder: 1
+      },
+      $ArrowNavigatorOptions: {
+        $Class: $JssorArrowNavigator$
+      },
+      $ThumbnailNavigatorOptions: {
+        $Class: $JssorThumbnailNavigator$,
+        $Cols: 10,
+        $SpacingX: 8,
+        $SpacingY: 8,
+        $Align: 360
+      }
+    };
+
+    var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+    /*responsive code begin*/
+    /*you can remove responsive code if you don't want the slider scales while window resizing*/
+    function ScaleSlider() {
+        var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+        if (refSize) {
+            refSize = Math.min(refSize, 800);
+            jssor_1_slider.$ScaleWidth(refSize);
+        }
+        else {
+            window.setTimeout(ScaleSlider, 30);
+        }
+    }
+    ScaleSlider();
+    $(window).bind("load", ScaleSlider);
+    $(window).bind("resize", ScaleSlider);
+    $(window).bind("orientationchange", ScaleSlider);
+    /*responsive code end*/
+});
